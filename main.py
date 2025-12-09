a5eaad0f3fb69c7eb999463c320fa6f9d2768631
Reeeeeeeeeehdjrb <147665800+Reeeeeeeeeehdjrb@users.noreply.github.com>
2025-12-09 17:43:53 -0600

    Added Uptime robot capibilities

--- main.py
+++ main.py
@@ -774,3 +774,19 @@ if __name__ == "__main__":
         print("Please add your Discord bot token to the Secrets.")
     else:
         bot.run(TOKEN)
+
+
+from flask import Flask
+from threading import Thread
+
+app = Flask("")
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+Thread(target=run).start()
+
