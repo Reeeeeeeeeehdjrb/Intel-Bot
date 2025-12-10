edc864a75aa246a3dc56fc6ebaf5d9cd76648708
Reeeeeeeeeehdjrb <147665800+Reeeeeeeeeehdjrb@users.noreply.github.com>
2025-12-09 18:11:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -774,20 +774,3 @@ if __name__ == "__main__":
         print("Please add your Discord bot token to the Secrets.")
     else:
         bot.run(TOKEN)
-
-
-from flask import Flask
-from threading import Thread
-import os
-
-app = Flask("")
-
-@app.route("/")
-def home():
-    return "Bot is running!"
-
-def run():
-    port = int(os.environ.get("PORT", 10000))  # Render assigns PORT
-    app.run(host="0.0.0.0", port=port)
-
-Thread(target=run).start()
